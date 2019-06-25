e72282836a98de3fff3afd6baa746c9239a44607
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-25 11:12:11 +0200

    Add new event listener in 'add new column' click

--- js/Board.js
+++ js/Board.js
@@ -8,15 +8,40 @@ var board = {
 };
 
 document.querySelector('#board .create-column').addEventListener('click', function() {
-    var name = prompt('Enter a column name');
-    var column = new Column(name);
-    board.addColumn(column);
+  var name = prompt('Enter a column name');
+  var data = new FormData();
+
+  data.append('name', name);
+
+  fetch(baseUrl + '/column', {
+      method: 'POST',
+      headers: myHeaders,
+      body: data,
+    })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(resp) {
+      var column = new Column(resp.id, name);
+      board.addColumn(column);
+    });
 });
-	
+
 function initSortable(id) {
-  	var el = document.getElementById(id);
-  	var sortable = Sortable.create(el, {
-    	group: 'kanban',
-    	sort: true
-  	});
+    var el = document.getElementById(id);
+    var sortable = Sortable.create(el, {
+      group: 'kanban',
+      sort: true
+    });
 }
+
+/*  ==============================
+  POST /column
+  ------------------------------
+  Request:
+  name: string - name of the column to create
+  ------------------------------
+  Response:
+  {
+     id: int
+  } */
