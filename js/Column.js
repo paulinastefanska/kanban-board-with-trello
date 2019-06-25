1d5ad2bc580873a41d16adcbb8b61d4bcb45de4d
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-25 11:03:10 +0200

    Change add and remove columns in Column.js

--- js/Column.js
+++ js/Column.js
@@ -14,16 +14,63 @@ function Column(id, name) {
 		}
 
 		if (event.target.classList.contains('add-card')) {
-			self.addCard(new Card(prompt("Enter the name of the card")));
-		}
+			var cardName = prompt("Enter the name of the card");
+			event.preventDefault();
+		  
+			var data = new FormData();
+			data.append('name', cardName);
+			data.append('bootcamp_kanban_column_id', self.id);
+
+			fetch(baseUrl + '/card', {
+			    method: 'POST',
+			    headers: myHeaders,
+			    body: data,
+			  })
+			  .then(function(res) {
+			    return res.json();
+			  })
+			  .then(function(resp) {
+			    var card = new Card(resp.id, cardName);
+			    self.addCard(card);
+			  });
+		}	  
 	});		
 } // Table column
+
+/*	POST /card
+	------------------------------
+	Request:
+	name: string - the name of the card we create
+	bootcamp_kanban_column_id: int - the id of the column to which the card belongs
+	------------------------------
+	Response:
+	{
+	   id: int
+	}	*/
 
 Column.prototype = {
     addCard: function(card) {
       this.element.querySelector('ul').appendChild(card.element);
     },
     removeColumn: function() {
-      this.element.parentNode.removeChild(this.element);
-    }
+	  var self = this;
+	  fetch(baseUrl + '/column/' + self.id, { method: 'DELETE', headers: myHeaders })
+	    .then(function(resp) {
+	      return resp.json();
+	    })
+	    .then(function(resp) {
+	      self.element.parentNode.removeChild(self.element);
+	    });
+	}
 };
+
+/*	DELETE /column/{id}
+	------------------------------
+	Request:
+	{id}: int - id column, we want to delete
+	------------------------------
+	Response:
+	{
+	   id: int
+	}	*/
+
