e1d703a25eb94a79380d4cfd8257eeab234c3257
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-25 11:06:34 +0200

    Change remove card in Card.js

--- js/Card.js
+++ js/Card.js
@@ -1,22 +1,45 @@
-function Card(description) {
-		var self = this;
+function Card(id, name) {
+	var self = this;
 
-		this.id = randomString();
-		this.description = description;
-		this.element = generateTemplate('card-template', { 
-			description: this.description }, 'li');
+	this.id = randomString();
+	this.name = name || 'No name given';
+	this.element = generateTemplate('card-template', { 
+		description: this.name }, 'li');
 
-		this.element.querySelector('.card').addEventListener('click', function (event) {
-			event.stopPropagation();
+	this.element.querySelector('.card').addEventListener('click', function (event) {
+		event.stopPropagation();
 
-			if (event.target.classList.contains('btn-delete')) {
-			    self.removeCard();
-			}
-		});
-	}
+		if (event.target.classList.contains('btn-delete')) {
+		    self.removeCard();
+		}
+	});
+}
 
-	Card.prototype = {
-		removeCard: function() {
-			this.element.parentNode.removeChild(this.element);
-	    }
-	}
+Card.prototype = {
+	removeCard: function() {
+		this.element.parentNode.removeChild(this.element);
+    }
+}
+
+removeCard: function() {
+  var self = this;
+
+  fetch(baseUrl + '/card/' + self.id, { method: 'DELETE', headers: myHeaders })
+    .then(function(resp) {
+      return resp.json();
+    })
+    .then(function(resp) {
+      self.element.parentNode.removeChild(self.element);
+    })
+}
+
+/*	DELETE /card/{id}
+	------------------------------
+	Request:
+	{id}: int - id card we want to remove
+	------------------------------
+	Response:
+	{
+	   id: int
+	}	*/
+
