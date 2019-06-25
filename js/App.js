861d38de00d0f8b8112821c3d354d1f0e525a8d6
Paulina Stefanska <web.paulinastefanska@gmail.com>
2019-06-25 10:27:31 +0200

    Add function to create cards in columns

--- js/App.js
+++ js/App.js
@@ -41,6 +41,21 @@ function setupColumns(columns) {
   	});
 }
 
+function setupColumns(columns) {
+  columns.forEach(function (column) {
+		var col = new Column(column.id, column.name);
+    board.addColumn(col);
+    setupCards(col, column.cards);
+  });
+}
+
+function setupCards(col, cards) {
+	cards.forEach(function (card) {
+    var cardObj = new Card(card.id, card.name);
+  	col.addCard(cardObj);
+	});
+}
+
 	// CREATING COLUMNS
 	var todoColumn = new Column('To do');
 	var doingColumn = new Column('Doing');
